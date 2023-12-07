2ed732505c8a5ec54dc097bc3b030b548035c48d
Roca <destiny.roca@fiserv.com>
2023-12-07 08:12:07 -0500

    got save function to work

--- backend.py
+++ backend.py
@@ -1,29 +1,18 @@
-from flask import Flask, request, jsonify, session
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Enable Cross-Origin Resource Sharing (CORS) for all routes
-CORS(app)
+# Route to render the HTML page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Set a secret key for session management
-app.secret_key = 'your_secret_key'
+# Toggle dark/light mode
+@app.route('/toggle-dark-mode')
+def toggle_dark_mode():
+    # Logic to toggle dark/light mode
+    # For simplicity, we will just return a message
+    return 'Dark mode toggled'
 
-# Define a route to handle toggling the dark/light mode
-@app.route('/toggle_mode', methods=['POST'])
-def toggle_mode():
-    # Access the current mode from the session, default to 'light'
-    current_mode = session.get('mode', 'light')
-
-    # Toggle the mode
-    new_mode = 'dark' if current_mode == 'light' else 'light'
-
-    # Update the session with the new mode
-    session['mode'] = new_mode
-
-    # Return a JSON response indicating success and the new mode
-    return jsonify({'status': 'success', 'mode': new_mode})
-
-# Run the app if this script is executed
 if __name__ == '__main__':
     app.run(debug=True)
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Function to add a new task
   function newElement() {
-    // Get the input value from the "new-task" input field
+    // Get the input values from the "new-task", "new-due-time", "new-due-date" input fields
     var inputValue = document.getElementById('new-task').value;
+    var dueDateValue = document.getElementById('new-due-date').value;
+    var dueTimeValue = document.getElementById('new-due-time').value;
 
     // Check if the input is empty
     if (inputValue === '') {
@@ -17,8 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
       checkbox.type = 'checkbox';
       li.appendChild(checkbox);
 
+      // Construct the task text with due date and time if available
+      var taskText = ' ' + inputValue;
+      if (dueDateValue !== '') {
+        taskText += ' (Due: ' + dueDateValue;
+        if (dueTimeValue !== '') {
+          // Convert the time to 12-hour format
+          var timeComponents = dueTimeValue.split(':');
+          var hours = parseInt(timeComponents[0]);
+          var minutes = timeComponents[1];
+          var ampm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12 || 12; // Convert 0 to 12 for midnight
+          taskText += ' at ' + hours + ':' + minutes + ' ' + ampm;
+        }
+        taskText += ')';
+      }
+
       // Add the task text to the list item
-      li.appendChild(document.createTextNode(' ' + inputValue));
+      li.appendChild(document.createTextNode(taskText));
 
       // Add a space between the text and buttons
       li.appendChild(document.createTextNode(' '));
@@ -40,6 +58,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // Clear the input field
       document.getElementById('new-task').value = '';
+      document.getElementById('new-due-date').value = '';
+      document.getElementById('new-due-time').value = '';
 
       // Set event listeners for the new task
       setEventListeners(li);
@@ -47,35 +67,50 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Function to move task to completed section
+  // Function to move task to completed section
   function taskCompleted() {
     // Get the parent list item of the checkbox
     var listItem = this.parentNode;
-    // Get the "completed-tasks" list
-    var completedTasksList = document.getElementById('completed-tasks');
 
-    // Check if the task is already in the completed section
-    if (listItem.parentNode === completedTasksList) {
-      return;
+    // Check if listItem exists and contains the required elements
+    if (listItem && listItem.querySelector && listItem.querySelector('input[type="checkbox"]') && listItem.querySelector('.edit') && listItem.querySelector('.delete')) {
+      //console.log('Found listItem and required elements:', listItem);
+
+      // Get the "completed-tasks" list
+      var completedTasksList = document.getElementById('completed-tasks');
+
+      // Check if the task is already in the completed section
+      if (listItem.parentNode === completedTasksList) {
+        return;
+      }
+
+      // Add strikethrough style to the task text
+      listItem.style.textDecoration = 'line-through';
+
+      // Remove existing 'Edit' and 'Delete' buttons
+      var existingEditButton = listItem.querySelector('.edit');
+      var existingDeleteButton = listItem.querySelector('.delete');
+
+      if (existingEditButton && existingDeleteButton) {
+        existingEditButton.parentNode.removeChild(existingEditButton);
+        existingDeleteButton.parentNode.removeChild(existingDeleteButton);
+
+        // Append the task to the "completed-tasks" list
+        completedTasksList.appendChild(listItem);
+
+        // Set event listeners for the completed task
+        setEventListeners(listItem);
+
+        // Disable the checkbox
+        this.checked = true;
+        this.disabled = true;
+      } else {
+        console.error('Error: existing buttons not found.');
+      }
+    } else {
+      console.error('Error: listItem or required elements not found.');
+      //console.log('listItem:', listItem);
     }
-
-    // Add strikethrough style to the task text
-    listItem.style.textDecoration = 'line-through';
-
-    // Remove existing 'Edit' and 'Delete' buttons
-    var existingEditButton = listItem.querySelector('.edit');
-    var existingDeleteButton = listItem.querySelector('.delete');
-    existingEditButton.parentNode.removeChild(existingEditButton);
-    existingDeleteButton.parentNode.removeChild(existingDeleteButton);
-
-    // Append the task to the "completed-tasks" list
-    completedTasksList.appendChild(listItem);
-
-    // Set event listeners for the completed task
-    setEventListenersForCompleted(listItem);
-
-    // Disable the checkbox
-    this.checked = true;
-    this.disabled = true;
   }
 
   // Function to edit task
@@ -83,67 +118,99 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the parent list item of the "Edit" button
     var listItem = this.parentNode;
 
-    // Remove existing 'Edit' and 'Delete' buttons
-    var existingEditButton = listItem.querySelector('.edit');
-    var existingDeleteButton = listItem.querySelector('.delete');
-    existingEditButton.parentNode.removeChild(existingEditButton);
-    existingDeleteButton.parentNode.removeChild(existingDeleteButton);
+    // Check if listItem exists and contains the required elements
+    if (listItem && listItem.querySelector && listItem.querySelector('.edit') && listItem.querySelector('.delete')) {
+      // Remove existing 'Edit' and 'Delete' buttons if they exist
+      var existingEditButton = listItem.querySelector('.edit');
+      var existingDeleteButton = listItem.querySelector('.delete');
 
-    // Create an input field for editing
-    var editInput = document.createElement('input');
-    editInput.type = 'text';
-    editInput.value = listItem.childNodes[1].nodeValue.trim();
-    listItem.replaceChild(editInput, listItem.childNodes[1]);
+      if (existingEditButton && existingDeleteButton) {
+        existingEditButton.parentNode.removeChild(existingEditButton);
+        existingDeleteButton.parentNode.removeChild(existingDeleteButton);
 
-    // Create "Save" button
-    var saveButton = document.createElement('button');
-    saveButton.className = 'edit';
-    saveButton.appendChild(document.createTextNode('Save'));
-    listItem.appendChild(saveButton);
+        // Create an input field for editing
+        var editInput = document.createElement('input');
+        editInput.type = 'text';
+        editInput.value = listItem.childNodes[1].nodeValue.trim();
+        listItem.replaceChild(editInput, listItem.childNodes[1]);
 
-    // Create "Delete" button
-    var deleteButton = document.createElement('button');
-    deleteButton.className = 'delete';
-    deleteButton.appendChild(document.createTextNode('Delete'));
-    listItem.appendChild(deleteButton);
+        // Create "Save" button
+        var saveButton = document.createElement('button');
+        saveButton.className = 'edit';
+        saveButton.appendChild(document.createTextNode('Save'));
+        listItem.appendChild(saveButton);
 
-    // Update the event listener for the "Save" button
-    saveButton.addEventListener('click', saveTask);
+        // Create "Delete" button
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.appendChild(document.createTextNode('Delete'));
+        listItem.appendChild(deleteButton);
 
-    // Set event listeners for the task during editing
-    setEventListeners(listItem);
+        // Remove the event listener for the "Save" button
+        saveButton.removeEventListener('click', saveTask);
+
+        // Update the event listener for the "Save" button
+        saveButton.addEventListener('click', function () {
+          saveTask(listItem);
+        });
+
+        // Set event listeners for the task during editing
+        setEventListeners(listItem);
+      } else {
+        console.error('Error: existing buttons not found.');
+      }
+    } else {
+      console.error('Error: listItem or required elements not found.');
+      //console.log('listItem:', listItem);
+    }
   }
 
   // Function to save edited task
-  function saveTask() {
-    // Get the parent list item of the "Save" button
-    var listItem = this.parentNode;
-    // Get the input field for editing
-    var editInput = listItem.querySelector('input[type="text"]');
-    // Create a text node with the edited content
-    var textNode = document.createTextNode(editInput.value);
+  function saveTask(listItem) {
+    // Check if listItem exists and contains the required elements
+    if (listItem && listItem.querySelector) {
+      // Get the input field for editing
+      var editInput = listItem.querySelector('input[type="text"]');
 
-    // Replace the original task text with the edited content
-    listItem.replaceChild(textNode, listItem.childNodes[1]);
+      if (editInput) {
+        // Create a text node with the edited content
+        var textNode = document.createTextNode(editInput.value);
 
-    // Remove the 'Save' and 'Delete' buttons
-    listItem.removeChild(listItem.lastChild); // 'Delete' button
-    listItem.removeChild(listItem.lastChild); // 'Save' button
+        // Replace the original task text with the edited content
+        listItem.replaceChild(textNode, listItem.childNodes[1]);
 
-    // Create "Edit" button
-    var editButton = document.createElement('button');
-    editButton.className = 'edit';
-    editButton.appendChild(document.createTextNode('Edit'));
-    listItem.appendChild(editButton);
+        // Remove the 'Save' and 'Delete' buttons
+        var saveButton = listItem.querySelector('.edit');
+        var deleteButton = listItem.querySelector('.delete');
 
-    // Create "Delete" button
-    var deleteButton = document.createElement('button');
-    deleteButton.className = 'delete';
-    deleteButton.appendChild(document.createTextNode('Delete'));
-    listItem.appendChild(deleteButton);
+        if (saveButton) {
+          listItem.removeChild(saveButton);
+        }
+        if (deleteButton) {
+          listItem.removeChild(deleteButton);
+        }
 
-    // Set event listeners for the task after editing
-    setEventListeners(listItem);
+        // Create "Edit" button
+        var editButton = document.createElement('button');
+        editButton.className = 'edit';
+        editButton.appendChild(document.createTextNode('Edit'));
+        listItem.appendChild(editButton);
+
+        // Create "Delete" button
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.appendChild(document.createTextNode('Delete'));
+        listItem.appendChild(deleteButton);
+
+        // Set event listeners for the task after editing
+        setEventListeners(listItem);
+      } else {
+        console.error('Error: editInput not found.');
+      }
+    } else {
+      console.error('Error: listItem or required elements not found.');
+      //console.log('listItem:', listItem);
+    }
   }
 
   // Function to delete task
@@ -165,12 +232,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Dark/Light mode toggle function
   function toggleMode() {
-    console.log('Toggle mode function called');
+    //console.log('Toggle mode function called');
     // Toggle the "dark-mode" class on the body element
     document.body.classList.toggle('dark-mode');
 
     // Add a fetch request to your Flask backend
-    fetch('http://127.0.0.1:5000/toggle_mode', {
+    fetch('http://localhost:5000/toggle_mode', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -178,7 +245,7 @@ document.addEventListener('DOMContentLoaded', function () {
     })
       .then(response => response.json())
       .then(data => {
-        console.log('Success:', data);
+        //console.log('Success:', data);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -188,33 +255,54 @@ document.addEventListener('DOMContentLoaded', function () {
   // Set event listener for the "Toggle Dark/Light Mode" button
   document.getElementById('mode-toggle').addEventListener('click', toggleMode);
 
+  // Set initial event listeners for existing completed tasks
+  var completedTasks = document.querySelectorAll('#completed-tasks li');
+  completedTasks.forEach(function (task) {
+    setTimeout(function () {
+      setEventListeners(task, true); // Pass true to indicate that these are completed tasks
+    }, 0);
+  });
+
+  // Set initial event listeners for existing tasks
+  var incompleteTasks = document.querySelectorAll('#incomplete-tasks li');
+  incompleteTasks.forEach(function (task) {
+    setTimeout(function () {
+      setEventListeners(task, false); // Pass false to indicate that these are incomplete tasks
+    }, 0);
+  });
+
   // Function to set event listeners for a task item
-  function setEventListeners(listItem) {
+  function setEventListeners(listItem, isCompleted) {
     var checkbox = listItem.querySelector('input[type="checkbox"]');
     var editButton = listItem.querySelector('.edit');
     var deleteButton = listItem.querySelector('.delete');
 
     checkbox.addEventListener('change', taskCompleted);
 
-    if (!checkbox.checked) {
-      // Only add event listeners for edit and delete buttons if the checkbox is not checked
-      editButton.addEventListener('click', editTask);
-      deleteButton.addEventListener('click', deleteTask);
+    // Check if listItem exists and contains the required elements
+    if (listItem && listItem.querySelector && checkbox) {
+      //console.log('Found listItem and required elements:', listItem);
+      //console.log('checkbox:', checkbox);
+
+      if (!isCompleted) {
+        // Only add event listeners for edit and delete buttons if the checkbox is not checked
+        if (editButton) {
+          editButton.addEventListener('click', editTask);
+        }
+        if (deleteButton) {
+          deleteButton.addEventListener('click', deleteTask);
+        }
+      }
+    } else {
+      console.error('Error: listItem or required elements not found.');
+      //console.log('listItem:', listItem);
+      //console.log('checkbox:', checkbox);
+      //console.log('editButton:', editButton);
+      //console.log('deleteButton:', deleteButton);
     }
   }
 
-  // Set initial event listeners for existing completed tasks
-  var completedTasks = document.querySelectorAll('#completed-tasks li');
-  completedTasks.forEach(function (task) {
-    setEventListeners(task);
-  });
-
-  // Set initial event listeners for existing tasks
-  var incompleteTasks = document.querySelectorAll('#incomplete-tasks li');
-  incompleteTasks.forEach(function (task) {
-    setEventListeners(task);
-  });
-
   // Event listener for the "Add" button
   document.querySelector('.add').addEventListener('click', newElement);
+
 });
